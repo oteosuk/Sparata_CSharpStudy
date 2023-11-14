194a66064cef5f4354512c3f5a2e1c469873ef39
oteosuk <oteosuk@naver.com>
2023-11-14 17:12:01 +0900

    2주차 완료

--- CSharpStudy/Program.cs
+++ CSharpStudy/Program.cs
@@ -1,17 +1,15 @@
 ﻿namespace CSharpStudy
 {
-    //using static Console;
+    using static Console;
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>();
-
-
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(i);
-            }
+            Write("이름을 입력하세요: ");
+            string name = ReadLine();
+            Write("나이를 입력하세요: ");
+            string age = ReadLine();
+            WriteLine("안녕하세요, {0}! 당신은 {1} 세 이군요.", name, age);
         }
     }
 }
